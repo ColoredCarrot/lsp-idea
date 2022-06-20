1958795e0bc503ef7ff017019df2a4093c1214b7
Julian K <7807570+ColoredCarrot@users.noreply.github.com>
2022-06-20 20:25:41 +0200

    Refactor InstantiableLspServer creation

--- src/main/kotlin/info/voidev/lspidea/config/servers/LspServersConfig.kt
+++ src/main/kotlin/info/voidev/lspidea/config/servers/LspServersConfig.kt
@@ -8,7 +8,12 @@ import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.service
 import com.intellij.util.xmlb.annotations.Tag
 import com.intellij.util.xmlb.annotations.XMap
+import info.voidev.lspidea.LspIdea
 import info.voidev.lspidea.LspSessionManager
+import info.voidev.lspidea.config.InstantiableLspServer
+import info.voidev.lspidea.config.serverhandler.LspRunnerConfigStateInterface
+import info.voidev.lspidea.config.serverhandler.LspRunnerType
+import info.voidev.lspidea.def.LspServerSupport
 import java.util.concurrent.atomic.AtomicBoolean
 
 @State(
@@ -31,7 +36,24 @@ internal class LspServersConfig : PersistentStateComponent<LspServersConfig.Stat
             var serverConfigProviderId: String = "",
             @Tag
             var state: LspServerConfigState = LspServerConfigState(),
-        )
+        ) {
+            fun createInstantiableServer(): InstantiableLspServer? {
+                val configProvider = LspServerSupport.getById(serverConfigProviderId) as LspServerSupport<LspServerOptionsConfigStateInterface>?
+                if (configProvider == null) {
+                    LspIdea.showError(
+                        "Missing Plugin",
+                        "The language server with the ID \"${state.id}\" references missing support provider \"${serverConfigProviderId}\". Either the storage file was corrupted or a previously installed plugin has been uninstalled.",
+                        null
+                    )
+                    return null
+                }
+
+                return InstantiableLspServer(
+                    configProvider.createConfiguredServer(state.options.get()),
+                    (state.runner.type!! as LspRunnerType<LspRunnerConfigStateInterface>).createRunnerProvider(state.runner.getConfig()!!)
+                )
+            }
+        }
     }
 
     private var state = State()
@@ -60,5 +82,8 @@ internal class LspServersConfig : PersistentStateComponent<LspServersConfig.Stat
 
         @JvmStatic
         fun get() = getInstance().state
+
+        @JvmStatic
+        fun getInstantiableServers() = get().servers.mapNotNull { (_, entry) -> entry.createInstantiableServer() }
     }
 }
